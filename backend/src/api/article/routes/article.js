d4751046aec63af4d9f5e0abcfb477af733b230b
mshahulpm <mshahulpm@gmail.com>
2022-03-20 16:29:09 +0530

    some more changes

--- backend/src/api/article/routes/article.js
+++ backend/src/api/article/routes/article.js
@@ -6,4 +6,11 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::article.article');
+module.exports = createCoreRouter('api::article.article', {
+    config: {
+        find: (ctx, next) => {
+            console.log(ctx.query);
+            next();
+        }
+    }
+});
